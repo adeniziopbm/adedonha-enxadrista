c4dd263479444f8a96546ceb9267d7db8ff90bd4
adeniziopbm <adenizio@iftm.edu.br>
2026-06-13 17:39:59 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "adedonha-fem-v2-1";
+const urlsToCache = ["./","./index.html","./styles.css","./app.js","./banco-termos.js","./manifest.json","./offline.html","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match("./offline.html"))));
+});
